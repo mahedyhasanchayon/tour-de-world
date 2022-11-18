ee18f1df874a11ffb02852ab391a85be364a24ee
mahedyhasanchayon <mahedy15-3004@diu.edu.bd>
2022-11-18 16:30:16 +0600

    Update Registration

---
+++ script.js
@@ -0,0 +1,26 @@
+var navBar= document.getElementById("navBar");
+
+        let signupBtn = document.getElementById("signupBtn");
+        let signinBtn = document.getElementById("signinBtn");
+        let nameField = document.getElementById("nameField");
+        let title = document.getElementById("title");
+
+
+        signinBtn.onclick = function(){
+            nameField.style.maxHeight = "0";
+            title.innerHTML = "Sign In";
+            signupBtn.classList.add("disable");
+            signinBtn.classList.remove("disable");
+        }
+
+        signupBtn.onclick = function(){
+            nameField.style.maxHeight = "60px";
+            title.innerHTML = "Sign Up";
+            signupBtn.classList.remove("disable");
+            signinBtn.classList.add("disable");
+        }
+
+
+        function togglebtn(){
+           navBar.classList.toggle("hidemenu");
+        }
